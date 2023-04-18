a220d021e87ffad7527be07b50d3ce4ce1f581f6
Nik Graf <nik@nikgraf.com>
2023-04-18 14:23:55 +0200

    chore: setup JSI for iOS

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,5 @@
-import { NativeModules, Platform } from 'react-native';
+declare function jsi_multiply(a: number): number;
 
-const LINKING_ERROR =
-  `The package 'react-native-opaque' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-const Opaque = NativeModules.Opaque
-  ? NativeModules.Opaque
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return Opaque.multiply(a, b);
+export function multiply(a: number): number {
+  return jsi_multiply(a);
 }
