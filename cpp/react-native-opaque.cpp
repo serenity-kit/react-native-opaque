26f70b8027d78178d9bcfa1843976eac7be918cd
Stefan Oestreicher <oestef@gmail.com>
2023-05-02 13:50:01 +0200

    chore: add cargo config and return result of rust call in jsi func

--- cpp/react-native-opaque.cpp
+++ cpp/react-native-opaque.cpp
@@ -1,25 +1,25 @@
 #include <jsi/jsilib.h>
 #include <jsi/jsi.h>
 #include "react-native-opaque.h"
-// sstream contains functions to manipulate strings in C++
 #include <sstream>
 
-// The namespace allows for syntactic sugar around the JSI objects. ex. call: jsi::Function instead of facebook::jsi::Function
 using namespace facebook;
+
+extern "C" double get_rust_answer();
 
 // We get the runtime from the obj-c code and we create our native functions here
 void installOpaque(jsi::Runtime &jsiRuntime)
 {
 	// printf("installOpaque called");
 	auto jsi_multiply = jsi::Function::createFromHostFunction(
-			jsiRuntime,
-			jsi::PropNameID::forAscii(jsiRuntime, "jsi_multiply"),
-			0,
-			[](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value
-			{
-				double res = 52;
-				return jsi::Value(res);
-			});
+		jsiRuntime,
+		jsi::PropNameID::forAscii(jsiRuntime, "jsi_multiply"),
+		0,
+		[](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value
+		{
+			double res = get_rust_answer();
+			return jsi::Value(res);
+		});
 
 	jsiRuntime.global().setProperty(jsiRuntime, "jsi_multiply", std::move(jsi_multiply));
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,3 +2,8 @@
 pub extern "C" fn hello_from_rust() {
     println!("hello from rust")
 }
+
+#[no_mangle]
+pub extern "C" fn get_rust_answer() -> f64 {
+    return 1337.0;
+}
